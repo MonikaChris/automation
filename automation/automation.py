adf76a134603a94128311d0160c26db5d095e738
Monika <monidavies@gmail.com>
2022-12-07 10:28:44 -0800

    Find phone numbers

--- automation/automation.py
+++ automation/automation.py
@@ -1,3 +1,17 @@
 import os
 import shutil
+import re
+
+# Get text
+with open('./assets/potential-contacts.txt', 'r') as reader:
+    text = reader.read()
+
+# Search text for phone numbers
+pattern_phone_num = r'(((\+)?\d{1,3}[\s-])?\(?\d{3}\)?[\s.-]\d{2,3}[\s.-]\d{4}(x\d+)?|(\d{10}))'
+pattern_phone = r'(?:(?:\+)?\d{1,3}[\s-])?\(?:?\d{3}\)?[\s.-]\d{2,3}[\s.-]\d{4}(?:x\d+)?|(?:\d{10})'
+
+phone_numbers = re.findall(pattern_phone, text)
+
+if __name__ == "__main__":
+    print(phone_numbers)
 

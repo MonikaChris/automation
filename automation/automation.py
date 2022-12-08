3b49c530d36e384ede69f934265f8dd99020f704
Monika <monidavies@gmail.com>
2022-12-07 19:36:52 -0800

    Adds function to remove existing numbers

--- automation/automation.py
+++ automation/automation.py
@@ -5,7 +5,7 @@ import re
 
 def get_file(filepath):
     """
-    Reads a file and returns its text
+    Reads a file and returns its text as a String.
     :param filepath: String
     :return: String
     """
@@ -23,13 +23,80 @@ def get_phone_numbers(text):
     # Search text for phone numbers
     pattern_phone_num = r'(?:(?:\+)?\d{1,3}[\s-])?\(?:?\d{3}\)?[\s.-]\d{2,3}[\s.-]\d{4}(?:x\d+)?|(?:\d{10})'
 
-    phone_numbers = re.findall(pattern_phone_num, text)
+    return re.findall(pattern_phone_num, text)
 
+
+def format_phone_numbers(phone_numbers):
+    """
+    Formats phone numbers in a list
+    :param phone_numbers: List
+    :return: List
+    """
     formatted_phone_nums = []
     for num in phone_numbers:
-        formatted_phone_nums.append(format_phone_num(num))
-
+        formatted_phone_nums.append(format_phone_number(num))
     return formatted_phone_nums
+
+
+def format_phone_number(num):
+    """
+    Formats a single phone number, handles extensions.
+    :param num: String
+    :return: String
+    """
+    # Check for extension
+    if 'x' in num:
+        number, extension = num.split('x')
+        number = format_main_num(number)
+        return number + 'x' + extension
+
+    else:
+        return format_main_num(num)
+
+
+def format_main_num(num):
+    """
+    Parses and truncates a phone number that has no extension. Returns last 10 digits separated by dashes.
+    :param num: String
+    :return: String
+    """
+    number = ''
+
+    # Get digits only
+    for i in range(len(num)):
+        if num[i].isnumeric():
+            number += num[i]
+
+    # Remove excess front digits
+    while len(number) > 10:
+        number = number[1:]
+
+    # Return formatted phone number
+    return number[:3] + '-' + number[3:6] + '-' + number[6:]
+
+
+def get_new_phone_numbers(new_file, old_file):
+    """
+    Extracts and formats phone numbers from a text file, but excludes phone numbers present in a second text file.
+    :param new_file: String
+    :param old_file: String
+    :return: List
+    """
+    # Read old data
+    with open(old_file, 'r') as reader:
+        old_lst = reader.read().splitlines()
+
+    # Read new data, extract phone numbers
+    new_data = get_file(new_file)
+    new_lst = get_phone_numbers(new_data)
+
+    # Remove old numbers from list of new numbers
+    for num in old_lst:
+        if num in new_lst:
+            new_lst.remove(num)
+
+    # Return formatted numbers
+    return format_phone_numbers(new_lst)
 
 
 def write_list_to_file(lst, filename, directory):
@@ -54,46 +121,19 @@ def write_list_to_file(lst, filename, directory):
     shutil.move(filename, directory)
 
 
-def format_main_num(num):
-    """
-    Parses and truncates a phone number. Returns last 10 digits separated by dashes.
-    :param num: String
-    :return: String
-    """
-    number = ''
-
-    # Get digits only
-    for i in range(len(num)):
-        if num[i].isnumeric():
-            number += num[i]
-
-    # Remove excess front digits
-    while len(number) > 10:
-        number = number[1:]
-
-    # Return formatted phone number
-    return number[:3] + '-' + number[3:6] + '-' + number[6:]
-
-
-def format_phone_num(num):
-    """
-    Formats a phone number, handles extensions.
-    :param num: String
-    :return: String
-    """
-    # Check for extension
-    if 'x' in num:
-        number, extension = num.split('x')
-        number = format_main_num(number)
-        return number + 'x' + extension
-
-    else:
-        return format_main_num(num)
-
-
 if __name__ == "__main__":
-    # Get phone numbers
+    # Extract all phone numbers, format numbers, write them to a text file
     text_content = get_file('./assets/potential-contacts.txt')
-    numbers = get_phone_numbers(text_content)
-    write_list_to_file(numbers, 'phone_numbers.txt', 'assets')
+    num_lst = get_phone_numbers(text_content)
+    form_num_lst = format_phone_numbers(num_lst)
+    write_list_to_file(form_num_lst, 'phone_numbers.txt', 'assets')
+
+    # Omit duplicate phone numbers - extract all phone numbers, return formatted phone numbers minus those already
+    # appearing in the existing-contacts file
+    num_lst_short = get_new_phone_numbers('./assets/potential-contacts.txt', './assets/existing-contacts.txt')
+    write_list_to_file(num_lst_short, 'phone_numbers_no_dupes.txt', 'assets')
+
+
+
+
 
